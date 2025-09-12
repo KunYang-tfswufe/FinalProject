c5bb6bdb63382928b9d42bfb710b1cb23ab7415e
KunYang-tfswufe <42212346@stu.tfswufe.edu.cn>
2025-09-12 21:14:19 +0800

    2025-09-12 21:14:19: udpate

--- Saffron_STM32_Core/main.py
+++ Saffron_STM32_Core/main.py
@@ -7,6 +7,6 @@ led = Pin(Pin.cpu.C13, Pin.OUT)
 # 显式地控制“亮”和“灭”，行为完全可预测
 while True:
     led.low()   # 亮
-    time.sleep_ms(200)
+    time.sleep_ms(3000)
     led.high()  # 灭
     time.sleep_ms(200)
